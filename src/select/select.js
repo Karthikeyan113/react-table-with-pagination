65dfc3dacc96f128e9d45db1f3f1d7e1ab0754e5
Karthikeyan Krishnamoorthy <karthikeyan.krishnamoorthy@adp.com>
2019-06-30 00:09:43 +0530

    add pagination

--- src/select/select.js
+++ src/select/select.js
@@ -3,7 +3,6 @@ import React from 'react'
 function Select(props) {
   return (
     <select onChange={(e) => props.onChange(e.target.value, props.category)}>
-      <option value=""></option>
       {props.items.map((item) => <option key={item} value={item}>{item}</option>)}
     </select>
   )
